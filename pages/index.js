515dc60b9870109d11ae0754deb777fe65dba4b6
jihyehann <haneh0714@sookmyung.ac.kr>
2021-08-15 03:50:15 +0900

    'second'

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,103 @@
-import Head from 'next/head'
+import Image from 'next/image';
+import css from "styled-jsx/css";
+import {useRouter} from "next/router";
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default function Login() {
+    const router = useRouter();
+    return(
+        <div className='container'>
+            {/* 로그인 */}
+            <Image src="/images/fittoss_logo.png" width={280} height={50} />
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <button className='kakaoTalkButton' onClick={() => {alert('카카오톡으로 시작하기'); router.push('/');}}>
+                <Image src="/images/kakaotalk.png" width={40} height={40}/>
+                <div className='kakaoTalkText'>카카오톡으로 시작하기</div>
+            </button>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+            <div className='inputDiv'>
+                <Image src="/icons/fi-rr-user.svg" width="30" height="30" />
+                <input className='inputText' type='text' />
+            </div>
+            <div className='inputDiv'>
+                <Image src="/icons/fi-rr-lock.svg" width="30" height="30" />
+                <input className='inputText' type='text' />
+            </div>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+            <button className='loginButton' onClick={() => {alert('로그인'); router.push('/');}}>
+                <div className='loginText'>로그인</div>
+            </button>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+            {/* style */}
+            <style jsx global>{`body{ margin:0px;}`}</style>
+            <style jsx>{style}</style>
+        </div> 
+    );
 }
+
+const style = css`
+    @font-face {  
+      font-family: 'GodoB';  
+      src: url('/fonts/GodoB.ttf');
+    }
+    .container{
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      background: url('/images/background30.png') no-repeat;
+      background-size: cover;
+    }
+    .kakaoTalkButton{
+      border: none;
+      background: #FAE100;
+      border-radius: 5px;
+      width: 350px;
+      height: 60px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 50px;
+      margin-bottom: 10px;
+    }
+    .kakaoTalkText{
+      font-family: 'GodoB';
+      color: #3C1E1E;
+      font-size: 18px;
+    }
+  .inputDiv{
+    background: white;
+    width: 320px;
+    height: 60px;
+    border-width: 2px;
+    border-color: #EDEDED;
+    display: flex;
+    align-items: center;
+    padding: 0 15px;
+    margin-bottom: 1px;
+  }
+  .inputText {
+    border: none;
+    outline:none;
+    margin-left: 15px;
+    width: 100%;
+    height: 30px;
+    font-size: 18px;
+  }
+  .loginButton{
+    background: #383838;
+    border-radius: 5px;
+    width: 350px;
+    height: 60px;
+    margin-top: 10px;
+  }
+  .loginText{
+    color: white;
+    font-size: 20px;
+    font-family: 'GodoB';
+  }
+   button:hover{
+     cursor: pointer;
+   }
+`;
+
