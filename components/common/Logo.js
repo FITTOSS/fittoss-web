4914312039fed08eb5170507e3e03f3f5bbb216e
jihyehann <haneh0714@sookmyung.ac.kr>
2021-08-17 01:40:02 +0900

    update design

--- components/common/Logo.js
+++ components/common/Logo.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default function Logo() {
-    return <Image src="/images/fittoss_logo.png" width={233} height={50}
+    return <Image src="/images/fittoss_logo.png" width={233} height={60}
                   style={{
                       boxShadow: '0px 3px 6px #00000029',
                       opacity: 1,
